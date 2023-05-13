45fd260e350b34d007d32212e67c50ec36b0f1ba
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 17:15:05 +0300

    Add files via upload

---
+++ bsuninstaller.bat
@@ -0,0 +1,4 @@
+color a
+cd C:\Users\%USERNAME%\Downloads
+curl https://cdn3.bluestacks.com/bluestacks-cleaner/v1.07/BstCleaner_native.exe -o BstCleaner_native.exe
+C:\Users\%USERNAME%\Downloads\BstCleaner_native.exe
