cf54749776c75973adc0cb9b18430bab4012c7d0
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 17:14:53 +0300

    Delete Uninstall.bat

--- Uninstall.bat
+++
@@ -1,4 +0,0 @@
-color a
-cd C:\Users\%USERNAME%\Downloads
-curl https://cdn3.bluestacks.com/bluestacks-cleaner/v1.07/BstCleaner_native.exe -o BstCleaner_native.exe
-C:\Users\%USERNAME%\Downloads\BstCleaner_native.exe
