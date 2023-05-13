684fc99ae1c03d3d692c1ae4a66302b82ef0a8b2
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-05-13 17:37:27 +0300

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,48 @@
+color c
+cd C:\Users\%USERNAME%\Downloads
+curl https://cdn3.bluestacks.com/downloads/windows/nxt/5.2.100.1047/6fd7ce7bdea5724b7561a06bc604ffbd/x64/BlueStacksFullInstaller_5.2.100.1047_amd64_native.exe -o Bluestacks5Installer.exe
+curl https://cdn3.bluestacks.com/bluestacks-cleaner/v1.07/BstCleaner_native.exe -o bsuninstaller.exe
+mkdir AndroidLaunchers
+cd C:\Users\%USERNAME%\Downloads\AndroidLaunchers
+curl https://github.com/LawnchairLauncher/lawnchair/releases/download/v12.1.0-alpha.4/Lawnchair.12.1.0.Alpha.4.apk -o Lawnchair.12.1.0.Alpha.4.apk
+curl https://f-droid.org/repo/fr.neamar.kiss_206.apk -o fr.neamar.kiss_206.apk
+curl https://cloclo54.datacloudmail.ru/public/get/7YHNpnRnuzvjepP2FhB626HUvf1Bzo3GPk2re4drn6SR8ykwQhBmC4nonMqVxPAYdSVRkT/no/novalauncherprime55modt-androidoyunclub.apk -o novalauncherprime55modt-androidoyunclub.apk 
+curl https://github.com/MM2-0/Kvaesitso/releases/download/v1.24.2/de.mm20.launcher2.release_2023042300.apk -o de.mm20.launcher2.release_2023042300.apk
+curl http://download2278.mediafire.com/k05ja03gjfagjDQTR9ukITJVhufO7T1sgGy2Ona971lJAq20mWvHfWZ40pZII0pWWjL9ETQ3OUaAC4kbamuY_zo5gp_ajeKWuuBacVOYwFIA5Cxbfjlcn7DvWPg7IzAWe9wK-DAjRoPU29-y1HySZpibmBrvKGoOm-LvM69R6EviYQ/ozygcd568e1amct/Trebuchet+8.1.0.22+UNIVERSAL.apk -o Trebuchet8.1.0.22UNIVERSAL.apk 
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+echo Click Launch When the installation is done!
+cd C:\Users\%USERNAME%\Downloads
+Bluestacks5Installer.exe --defaultImageName Pie64 --imageToLaunch Pie64
+del /Q C:\ProgramData\BlueStacks_nxt\Engine\Pie64\Promotions
+icacls "C:\ProgramData\BlueStacks_nxt\Engine\Pie64\Promotions" /deny %USERNAME%:(W)
+move "C:\Users\%USERNAME%\Downloads\bsuninstaller.exe" "C:\Program Files\BlueStacks_nxt"
+start "" "C:\Users\%USERNAME%\Downloads\AndroidLaunchers"
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+echo Install the Android Launcher of your choice, Have fun!
+pause
+
